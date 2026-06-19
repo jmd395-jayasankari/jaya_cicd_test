a9f9cc542e47967c28c021fc1a1a43969cb29b64
jmd395-jayasankari <jayasankari.r@jmangroup.com>
2026-06-19 13:48:06 +0530

    Update customer_contract.sql

--- snowball_dbt-main/models/03_analysis/customer_contract.sql
+++ snowball_dbt-main/models/03_analysis/customer_contract.sql
@@ -1,33 +1,33 @@
-{{ 
-    config(
-        tags=['analysis']
-        ) 
-}}
- 
-/* This Stored Procedure calculates the join month, end month, and churn month for each customer based on their revenue records*/
+WITH filtered_revenue AS (
+
+    SELECT
+        customer_level_1,
+        customer_level_2,
+        customer_level_3,
+
+        month_roll,
+        arr,
+        revenue_type
+
+    FROM monthly_revenue
+
+    WHERE arr <> 0.0
+      AND revenue_type IN ('Recurring', 'Re-occurring')
+)
 
 SELECT
-    
-    {{ get_dimension_from_table('monthly_revenue', 'customer_level') }} 
-    , MIN(month_roll)                                                 AS customer_join_month
-    , MAX(month_roll)                                                 AS customer_end_month
-    , DATEADD(MONTH, 1, MAX(month_roll))                              AS customer_churn_month
 
-FROM {{ ref('monthly_revenue') }}
-FROM {{ ref('monthly_revenue') }}
-    FROM {{ ref('monthly_revenue') }}
+    customer_level_1,
+    customer_level_2,
+    customer_level_3,
 
-FROM {{ ref('monthly_revenue') }}
-FROM {{ ref('monthly_revenue') }}
-    FROM {{ ref('monthly_revenue') }}
-WHERE
-    arr <> 0.0
-    AND revenue_type IN ('Recurring','Re-occurring')
+    MIN(month_roll) AS customer_join_month,
+    MAX(month_roll) AS customer_end_month,
+    DATEADD(MONTH, 1, MAX(month_roll)) AS customer_churn_month
 
-WHERE
-    arr <> 0.0
-    AND revenue_type IN ('Recurring','Re-occurring')
+FROM filtered_revenue
 
-GROUP BY 
-    {{ get_dimension_from_table('monthly_revenue', 'customer_level') }} 
-
+GROUP BY
+    customer_level_1,
+    customer_level_2,
+    customer_level_3;
