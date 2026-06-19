654a56b036f1d367036b6fcb090cb45cfc86eddc
Jayasankari R R <jayasankari.r@jmangroup.com>
2026-06-19 16:09:18 +0530

    feat(dbt): test

--- snowball_dbt-main/models/03_analysis/monthly_revenue.sql
+++ snowball_dbt-main/models/03_analysis/monthly_revenue.sql
@@ -1,5 +1,5 @@
 SELECT
     1 AS id,
     'dummy' AS name,
-    CURRENT_DATE() AS created_date
-    CURRENT_TIMESTAMP() AS now
+    --CURRENT_DATE() AS created_date
+    --CURRENT_TIMESTAMP() AS now
