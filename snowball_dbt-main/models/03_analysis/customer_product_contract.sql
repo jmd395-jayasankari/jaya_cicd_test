149c17c404b954360ccfa71a48272ff420d1bc89
jmd395-jayasankari <jayasankari.r@jmangroup.com>
2026-06-19 13:46:51 +0530

    Update customer_product_contract.sql

--- snowball_dbt-main/models/03_analysis/customer_product_contract.sql
+++ snowball_dbt-main/models/03_analysis/customer_product_contract.sql
@@ -1,45 +1,64 @@
-{{ 
-    config(
-        tags=['analysis']
-        ) 
-}}
- 
- /* This stored procedure calculates the start, end, and anticipated churn months for each customer-product pair based on recurring monthly_revenue data and product details*/
-
 WITH get_product_start_end_month AS (
 
-    SELECT
+    SELECT DISTINCT
 
-        {{ get_dimension_from_table('monthly_revenue', 'customer_level') }} 
-        , {{ get_dimension_from_table('monthly_revenue', 'product_level') }}   
-        , MIN(month_roll) OVER (PARTITION BY  {{ get_dimension_from_table('monthly_revenue', 'customer_level') }} , {{ get_dimension_from_table('monthly_revenue', 'product_level') }} )                 AS product_start_month
-        , MAX(month_roll) OVER (PARTITION BY  {{ get_dimension_from_table('monthly_revenue', 'customer_level') }} , {{ get_dimension_from_table('monthly_revenue', 'product_level') }} )                 AS product_end_month
-        , DATEADD(MONTH, 1, MAX(month_roll) OVER (PARTITION BY customer_level_1, product_level_1))                                                                                                       AS product_churn_month
-    
-    FROM {{ ref('monthly_revenue') }} AS r
-    WHERE
-        arr <> 0
-        AND revenue_type IN ('Recurring','Re-occurring')
-     WHERE
-        arr <> 0
-        AND revenue_type IN ('Recurring','Re-occurring')
+        customer_level_1,
+        customer_level_2,
+        product_level_1,
+        product_level_2,
+        entity_level_1,
+        entity_level_2,
+        other_level_1,
+        other_level_2,
+
+        MIN(month_roll) OVER (
+            PARTITION BY 
+                customer_level_1,
+                customer_level_2,
+                product_level_1,
+                product_level_2
+        ) AS product_start_month,
+
+        MAX(month_roll) OVER (
+            PARTITION BY 
+                customer_level_1,
+                customer_level_2,
+                product_level_1,
+                product_level_2
+        ) AS product_end_month,
+
+        DATEADD(
+            MONTH,
+            1,
+            MAX(month_roll) OVER (
+                PARTITION BY 
+                    customer_level_1,
+                    customer_level_2,
+                    product_level_1,
+                    product_level_2
+            )
+        ) AS product_churn_month
+
+    FROM monthly_revenue
+
+    WHERE arr <> 0
+      AND revenue_type IN ('Recurring', 'Re-occurring')
 
 )
 
-SELECT
-    
-    {{ get_dimension_from_table('monthly_revenue', 'customer_level') }} 
-    , {{ get_dimension_from_table('monthly_revenue', 'product_level') }}   
-    , product_start_month
-    , product_end_month
-    , product_churn_month
+SELECT DISTINCT
 
-FROM 
-    get_product_start_end_month
+    customer_level_1,
+    customer_level_2,
+    product_level_1,
+    product_level_2,
+    entity_level_1,
+    entity_level_2,
+    other_level_1,
+    other_level_2,
 
-GROUP BY
-    {{ get_dimension_from_table('monthly_revenue', 'customer_level') }} 
-    , {{ get_dimension_from_table('monthly_revenue', 'product_level') }}   
-    , product_start_month
-    , product_end_month
-    , product_churn_month
+    product_start_month,
+    product_end_month,
+    product_churn_month
+
+FROM get_product_start_end_month;
