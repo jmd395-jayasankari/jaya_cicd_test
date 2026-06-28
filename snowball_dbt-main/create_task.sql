54cf279e2788021f7f76f6374504a626dc6553c2
Jayasankari R R <jayasankari.r@jmangroup.com>
2026-06-28 09:07:29 +0530

    testing:task

--- snowball_dbt-main/create_task.sql
+++ snowball_dbt-main/create_task.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE TASK my_trial_db.cicd_test."03_analysis_execution"
 WAREHOUSE = COMPUTE_WH
+SCHEDULE = 'USING CRON 0 0 1 1 * UTC'
 AS
 EXECUTE dbt project DBT_SNOWBALL
 ARGS = 'build --select models/03_analysis --target dev';
