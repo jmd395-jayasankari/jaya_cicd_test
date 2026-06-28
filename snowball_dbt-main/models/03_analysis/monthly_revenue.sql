f1a7255b92f247f2f3f32137edc68e4184ff6b18
Jayasankari R R <jayasankari.r@jmangroup.com>
2026-06-28 19:48:36 +0530

    testing:task1

--- snowball_dbt-main/models/03_analysis/monthly_revenue.sql
+++ snowball_dbt-main/models/03_analysis/monthly_revenue.sql
@@ -3,4 +3,5 @@ SELECT
     'FINAL_CHECK' AS name,
     CURRENT_DATE() AS created_date,
     CURRENT_TIMESTAMP() AS updated_time,
-    'new_column_value' AS new_col
+    'new_column_value' AS new_col, 
+    'task_created_new' as new
