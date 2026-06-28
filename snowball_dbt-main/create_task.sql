2f0ca9d46251c56660bab7ad8a3a29ab6b25ef24
Jayasankari R R <jayasankari.r@jmangroup.com>
2026-06-28 18:50:34 +0530

    testing:task1

--- snowball_dbt-main/create_task.sql
+++ snowball_dbt-main/create_task.sql
@@ -3,4 +3,5 @@ WAREHOUSE = COMPUTE_WH
 SCHEDULE = 'USING CRON 0 0 1 1 * UTC'
 AS
 EXECUTE dbt project DBT_SNOWBALL
-ARGS = 'run --target dev --full-refresh';
+-- ARGS = 'run --target dev --full-refresh';
+ARGS = 'build --select 03_analysis --target dev';
